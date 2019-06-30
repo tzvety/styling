ce1f0fff630a395a4c18099b718eefe8d0bdee20
tzvety <tzvety@mail.com>
2019-06-30 02:44:59 +0100

    Colors. Borders ready

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # styling
+
+1. There is an error in Nunjucks-webpack-plugin. Need to be fixed manually (`~128 line`) as `compilation.fileDependencies` is a  `set`.
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,54 @@
 const path = require('path');
 const pages = require('./dev/pages');
 
-
 module.exports = {
-  mode: 'none',
-  entry: './dev/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    mode: 'none',
+    entry: './dev/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
 
-  devServer: {
-    publicPath: '/', // Live-reload
-    contentBase: 'dist/', // Relative directory for base of server
-    inline: true,
-    port: 3000,
-    historyApiFallback: true
-  },
+    devServer: {
+        publicPath: '/', // Live-reload
+        contentBase: 'dist/', // Relative directory for base of server
+        inline: true,
+        port: 3000,
+        historyApiFallback: true
+    },
 
-  plugins: [...pages.generatePages(path.resolve(__dirname, path.join(__dirname, './dev/views')))],
+    plugins: [...pages.generatePages(path.resolve(__dirname, path.join(__dirname, './dev/views')))],
 
-  module: {
-    
-    rules: [
-
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
+    module: {       
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    { loader: 'style-loader' }, // Adds CSS to the DOM by injecting a `<style>` tag
+                    { loader: 'css-loader' }, // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                    { 
+                        loader: 'postcss-loader', // Loader for webpack to process CSS with PostCSS
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    { loader: 'sass-loader' } // Loads a SASS/SCSS file and compiles it to CSS
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }]
             }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
         ]
-      }
-    ]
-  }
+    }
 };
